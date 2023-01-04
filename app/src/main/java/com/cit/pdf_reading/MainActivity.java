c74dbcc0fbfdd6c007a10733dd3c8efa5af1a0a4
Md. Ariful ISlam <arifewucse2016@gmail.com>
2023-01-04 11:10:54 +0600

    m

---
+++ app/src/main/java/com/cit/pdf_reading/MainActivity.java
@@ -0,0 +1,81 @@
+package com.cit.pdf_reading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import es.voghdev.pdfviewpager.library.RemotePDFViewPager;
+import es.voghdev.pdfviewpager.library.adapter.PDFPagerAdapter;
+import es.voghdev.pdfviewpager.library.remote.DownloadFile;
+import es.voghdev.pdfviewpager.library.util.FileUtil;
+
+public class MainActivity extends AppCompatActivity implements DownloadFile.Listener{
+
+    private RemotePDFViewPager remotePDFViewPager;
+
+    private PDFPagerAdapter pdfPagerAdapter;
+
+    private String url = "https://firebasestorage.googleapis.com/v0/b/social-media-7c551.appspot.com/o/Kotlin%20Android%20App%20Development%20Course%20Module.pdf?alt=media&token=87a0d67e-3bf2-436f-be04-2b95e8873531";
+
+
+    private ProgressBar progressBar;
+
+    private LinearLayout pdfLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //set the Visibility of the progressbar to visible
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        //initialize the pdfLayout
+        pdfLayout = findViewById(R.id.pdf_layout);
+
+
+
+        //Create a RemotePDFViewPager object
+        remotePDFViewPager = new RemotePDFViewPager(this, url, this);
+    }
+
+    @Override
+    public void onSuccess(String url, String destinationPath) {
+
+        // That's the positive case. PDF Download went fine
+        pdfPagerAdapter = new PDFPagerAdapter(this, FileUtil.extractFileNameFromURL(url));
+        remotePDFViewPager.setAdapter(pdfPagerAdapter);
+        updateLayout();
+        progressBar.setVisibility(View.GONE);
+    }
+
+    private void updateLayout() {
+
+        pdfLayout.addView(remotePDFViewPager,
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    public void onFailure(Exception e) {
+        // This will be called if download fails
+    }
+
+    @Override
+    public void onProgressUpdate(int progress, int total) {
+        // You will get download progress here
+        // Always on UI Thread so feel free to update your views here
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (pdfPagerAdapter != null) {
+            pdfPagerAdapter.close();
+        }
+    }
+}
